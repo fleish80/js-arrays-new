6f06c7ecb4803cbbb67309d42edca5dfe9766864
fleish80 <Fleish.D@gmail.com>
2023-06-18 09:32:15 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-arrays-new
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-x7qs6q)
---
+++ index.ts
@@ -0,0 +1,32 @@
+// Import stylesheets
+import './style.css';
+
+withFunc();
+sortFunc()
+
+// Write TypeScript code!
+function withFunc() {
+  const peopleWith = ['Sally', 'Kyle', 'John'];
+  const newPeopleWith = peopleWith.with(1, 'New');
+
+  console.log({peopleWith}, {newPeopleWith});
+}
+
+function sortFunc() {
+  const peopleSorted = ['Sally', 'Kyle', 'John'];
+  const newPeopleSorted= peopleSorted.sort();
+
+  console.log({peopleSorted}, {newPeopleSorted});
+
+  //before
+  const peopleSortedBefore = ['Sally', 'Kyle', 'John'];
+  const newPeopleSortedBefore= [...peopleSorted].sort();
+
+  console.log({peopleSortedBefore}, {newPeopleSortedBefore});
+
+  const peopleToSortedAfter = ['Sally', 'Kyle', 'John'];
+  const newPeopleToSortedAfter = peopleSorted.toSorted();
+
+  console.log({peopleToSortedAfter}, {newPeopleToSortedAfter});
+
+}
